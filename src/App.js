fbbe0273ca1476210e2f19533fa771e3f587c714
AYUSH KUMAR <ak0735000@gmail.com>
2022-04-22 15:55:03 +0530

    assign1 part1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Assign1 } from './assign1';
+
+function App() {
+  return (
+    <div className="App">
+      <Assign1/>
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assign1.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Assign1 = () => {
+  return (
+    <div>
+        <p>Mobile Operation System</p>
+        <ul>
+            <li>Andriod</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Window Phone</li>
+        </ul>
+        <p>Mobile Manufacturers</p>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+  )
+}
