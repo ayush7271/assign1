8ee17cd0cb83a8b38dc19792029c4d840cabb592
AYUSH KUMAR <ak0735000@gmail.com>
2022-04-22 19:05:49 +0530

    part2 update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Assign1 } from './assign1';
+import { Assignpart2 } from './Assignpart2';
 
 function App() {
   return (
     <div className="App">
-      <Assign1/>
+      {/* <Assign1/> */}
+      <Assignpart2/>
       
 
     </div>
---
+++ src/Assignpart2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Assignpart2 = () => {
+  return (
+    <div>
+        <button className='joinus'  >JOIN US</button>
+        <button className='setting' >SETTING</button>
+        <button className='login' >LOGIN</button>
+        <button className='contact' >CONTACT US</button>
+        <button className='search' >SEARCH</button>
+        <button className='help' >HELP</button>
+        <button className='home' >HOME</button>
+        <button className='download' >DOWNLOAD</button>
+    </div>
+  )
+}
